b7ece1e7d0fe6f1d333b9e6866784bde8cdb2445
sagarsp22222 <46356383+sagarsp22222@users.noreply.github.com>
2022-08-31 09:56:31 +0530

    Java Basic Code

---
+++ sp.java
@@ -0,0 +1,33 @@
+public class sp{
+	
+	static char a;
+	static int b;      
+    static byte c;
+    static double d;
+
+     public static void main(String []args){
+        System.out.println(" This is a List");
+		
+		System.out.println("char is not showing coz it's null" + a);
+		System.out.println("This is int default" +  b);
+		System.out.println("This is byte default" +  c);
+		System.out.println("This is double default" +  d);
+		
+		/*byte Quantity = 10;
+		char Product = 'J';
+		int SlNo = 1;
+		long fassai = 987654321;
+		double other = 20;
+		short OONumber = 57;
+		boolean verify = true;
+		
+		//System.out.println(SlNo, Quantity, Product, fassai, other, OONumber, verify);
+		System.out.println(SlNo);
+		System.out.println(Quantity);
+		System.out.println(Product);
+		System.out.println(fassai);
+		System.out.println(other);
+		System.out.println(OONumber);
+		System.out.println(verify);*/
+     }
+}
