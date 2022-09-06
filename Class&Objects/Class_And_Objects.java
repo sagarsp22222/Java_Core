f9ecf26b51405b9aa43419ddd525083beba8cc67
sagarsp22222 <46356383+sagarsp22222@users.noreply.github.com>
2022-09-06 08:29:19 +0530

    Class & Objects

---
+++ Class&Objects/Class_And_Objects.java
@@ -0,0 +1,15 @@
+class Class_And_Objects {
+           String _Laptop = "Asus Tuf Gaming A15";
+		   
+           char color = 'B';
+           byte classroom = 6;
+           byte sections = 2;
+           int lecturers = 15;
+           int subject = 6;
+           int attenders = 5;
+           int bus = 1;
+           int labs = 3;
+           int students = 50;
+           
+   
+}
---
+++ Class&Objects/Class_Objects_Connected.java
@@ -0,0 +1,13 @@
+class Class_Objects_Connected {
+                   public static void main(String [] str){
+					   
+                   Class_And_Objects c = new Class_And_Objects ();
+                   
+                    System.out.println(c.color);
+                    System.out.println(c._laptop);
+                    System.out.println(c._coreThreats);
+                    System.out.println(c._processorCores);
+                    System.out.println(c._processor);
+                    System.out.println(c._ram);
+      }
+}
