25a64def0c3152ef9453321ee441030c4dad7f27
sagarsp22222 <46356383+sagarsp22222@users.noreply.github.com>
2022-09-06 08:34:39 +0530

    if_else_Example

---
+++ Conditions/if_else_Example.java
@@ -0,0 +1,16 @@
+class if_else_Example { 
+        public static void main (String [] str){
+
+          int _expected_No= 100; 
+          int _actual_No = 80;
+        
+        if (_expected_No < _actual_No){
+            System.out.println ( "its a CAR");
+         }
+        else {
+            System.out.println (" its  not a car,its a BIKE");
+        }
+
+       
+    }
+}
