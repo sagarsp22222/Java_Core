875100de4c58ba56c3d5b6b29fc4d634828e772a
sagarsp22222 <46356383+sagarsp22222@users.noreply.github.com>
2022-09-06 08:32:34 +0530

    Conditions

---
+++ Conditions/if_Statement_Example.java
@@ -0,0 +1,17 @@
+class if_Statement_Example {
+            public static void main (String [] str)
+         {
+            int a = 100;
+            int b = 150;
+			
+            if (a > b) {
+                System.out.println(" No It's Wrong");
+            }
+            else if (a < b){
+                 System.out.println(" a is lesser than b");
+            }
+            else{
+                 System.out.println(" a greater than b");
+            }
+         }
+}
