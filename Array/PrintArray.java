524e72d1cd5457a754d6b61fefb443ccab910497
sagarsp22222 <46356383+sagarsp22222@users.noreply.github.com>
2022-09-06 08:19:50 +0530

    Array Added

---
+++ Array/PrintArray.java
@@ -0,0 +1,34 @@
+class PrintArray{
+         public static void main(String [] str){
+                 char _name1 = 'a';
+                 char _name2 = 'b';
+                 char _name3 = 'c';
+                 char _name4 = 'd';
+                 char _name5 = 'e';
+                 char _name6 = 'f';
+                   System.out.println(_name1 +"/*This is Empty*/"+ _name2 +""+ _name3 +"/*This is Empty*/"+ _name4 +"/*This is Empty*/"+ _name5 +""+ _name6);
+
+                 char [] _name = {'s','s','p'};
+                 System.out.println( _name );
+                 System.out.println(name [1] );
+
+                 float [] _Printfloat =  { 4.8f ,7.6f ,5.5f ,8.9f  };
+                 System.out.println( _Printfloat);
+
+                 int [] _PrintInt =  {45 ,56 ,98 ,67 ,55 };
+                 System.out.println( _PrintInt);
+
+                 short [] _PrintShort = { 3 ,5 ,6 ,8 ,1 };
+                 System.out.println(_PrintShort);
+
+                 double [] _PrintDouble =  { 78.2d , 54.8d ,88.6d , 65.8d};
+                 System.out.println(_PrintDouble);
+
+                 long [] _PrintLong = { 34567l , 4563728l , 87653428l, 56743l };
+                 System.out.println(_PrintLong);
+
+                 boolean [] _PrintBool = {true};
+                 System.out.println(_PrintBool);
+
+           }
+}
