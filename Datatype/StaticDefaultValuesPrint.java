d28aef424e6739db1cd91d0ca5a72ab07cb14b7f
sagarsp22222 <46356383+sagarsp22222@users.noreply.github.com>
2022-09-06 07:55:48 +0530

    Datatypes

---
+++ Datatype/StaticDefaultValuesPrint.java
@@ -0,0 +1,32 @@
+class StaticDefaultValuesPrint{
+           static Byte _size ;    
+           static short _short;
+           static int _int;
+           static long _long;
+           static double _double ;
+           static char _character;
+           static boolean _IsTrue ;
+           static float _float;   
+
+
+           public static void main(String [] args)
+             {
+            System.out.println("print the default value");
+             System.out.println(_size);
+
+             System.out.println(_short);
+
+             System.out.println( _int );
+
+             System.out.println( _long );
+
+             System.out.println( _double);
+             
+             System.out.println(_character);
+          
+             System.out.println(_IsTrue); 
+             
+             System.out.println(_float); 
+             
+          }
+}
