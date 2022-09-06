cb5dea57632595029f91ed863e693e15639f137a
sagarsp22222 <46356383+sagarsp22222@users.noreply.github.com>
2022-09-06 08:08:13 +0530

    Declaring And Init Variable

---
+++ Datatype/Declaring_And_Init_Variable.java
@@ -0,0 +1,21 @@
+class Declaring_And_Init_Variable{
+         public static void main(String [] str){
+                 System.out.println("starting of the main method");
+            //declaring variable
+            Byte  _age;
+
+            //initializing var
+            age = 22;
+                 System.out.println(_age);
+
+            //declaring and init in same line
+            char _lastname = 'Sp';
+                   System.out.println(_lastname);
+
+            boolean _isPassed = false;
+                  System.out.println( _isPassed);
+
+             
+         }
+}
+
