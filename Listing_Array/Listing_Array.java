7e929877ac0fbadf8453da671dedb1f361615275
sagarsp22222 <46356383+sagarsp22222@users.noreply.github.com>
2022-10-10 07:35:53 +0530

    Array Lists

---
+++ Listing_Array/Listing_Array.java
@@ -0,0 +1,34 @@
+package com.ssp.collection;
+import java.util.ArrayList;
+
+
+
+public class Names Listing_Array {
+
+public static void main(String[] args) {
+
+
+Listing_Array _ls= new Listing_Array();
+/* Below are array Lists */
+
+_ls.add("Sam"); // This Array is 0
+_ls.add("Leo");  // This Array is 1
+_ls.add("White guy");  // This Array is 2
+_ls.add(1998);  // This Array is 3
+_ls.add(6.2);  // This Array is 4
+
+System.out.println(_ls.get(2));
+System.out.println(_ls);
+_ls.remove(4); I
+
+System.out.println(_ls);
+System.out.println(_ls.size());
+
+
+_ls.clear();
+
+System.out.println(_ls);
+}
+
+
+}
